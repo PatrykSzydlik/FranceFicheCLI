71ebc0a0ee472f3406fa76084622535f3a9027e0
PatrykSzydlik <patrykszydlik3@gmail.com>
2023-04-23 13:12:27 +0200

    CLI functions update

--- FraFic.py
+++ FraFic.py
@@ -6,54 +6,38 @@ import os
 from datetime import datetime
 from enum import Enum
 
-# define enum for types of words in french
-class WordType(Enum):
-    NOUN = 1
-    VERB = 2
-    ADJECTIVE = 3
-    ADVERB = 4
-    PRONOUN = 5
-    PREPOSITION = 6
-    CONJUNCTION = 7
-    INTERJECTION = 8
+class grammar():
+    # define used languages for translation
+    Languages = ['french',
+                 'english',
+                 'polish']
+    
+    # define enum for types of words in french
+    WordType = ["NOUN", 
+                "VERB", 
+                "ADJECTIVE", 
+                "ADVERB", 
+                "PRONOUN",
+                "PREPOSITION", 
+                "CONJUNCTION",
+                "INTERJECTION"]
+    
+    # define enum for types of verbs in french
+    VerbType = {
+    "ER": "regular verbs that end in -er (e.g. parler, aimer)",
+    "IR": "verbs that end in -ir and are conjugated like finir (e.g. choisir, réagir)",
+    "RE": "irregular verbs that end in -ir (e.g. partir, sortir)",
+    "AUXILIARY": "irregular verbs that end in -re (e.g. prendre, mettre)"
+    }
 
-# define enum for types of verbs in french
-class VerbType(Enum):
-    ER = "Verbs ending in -er"
-    IR = "Verbs ending in -ir"
-    RE = "Verbs ending in -re"
-    AUXILIARY = "Auxiliary verbs"
-
-# Write dictionary explaining each type of verb in french
-VerbTypeExplained = {
-    VerbType.ER: "regular verbs that end in -er (e.g. parler, aimer)",
-    VerbType.IR: "verbs that end in -ir and are conjugated like finir (e.g. choisir, réagir)",
-    VerbType.RE: "irregular verbs that end in -ir (e.g. partir, sortir)",
-    VerbType.AUXILIARY: "irregular verbs that end in -re (e.g. prendre, mettre)"
-}
-
-# define enum for types of nouns in french
-class NounType(Enum):
-    MASCULINE = 1
-    FEMININE = 2
-    MASCULINE_PLURAL = 3
-    FEMININE_PLURAL = 4
-
-
-# define enum for types of adjectives in french
-class AdjectiveType(Enum):
-    QUALIFICATIF = "qualificatif"
-    NUMERAL = "numéral"
-    DEMONSTRATIF = "démonstratif"
-    INTERROGATIF = "interrogatif"
-    EXCLAMATIF = "exclamatif"
-    INDEFINI = "indéfini"
-    POSSESSIF = "possessif"
-    RELATIF = "relatif"
-    NEGATIF = "negatif"
-
-# As a comment explain what are the types of adjectives in french
-AdjectiveTypeExplained = {
+    # define enum for types of nouns in french
+    NounType = ["MASCULINE",
+                "FEMININE",
+                "MASCULINE_PLURAL",
+                "FEMININE_PLURAL"]
+    
+    # define enum for types of adjectives in french
+    AdjectiveType = {
     "qualificatif": "Adjectives of quality: describe the quality or characteristic of a noun.",
     "numéral": "Adjectives of quantity: indicate the quantity or amount of a noun.",
     "possessif": "Possessive adjectives: show ownership or possession of a noun.",
@@ -63,23 +47,73 @@ AdjectiveTypeExplained = {
     "exclamatif": "Exclamatory adjectives: express strong emotions or feelings about a noun.",
     "relatif":"Relative adjectives: describe a noun's relationship to another noun. ",
     "negatif": "Negative adjectives: describe or imply a negative meaning to a noun"
-}
+    }
 
-# define enum for types of adverbs in french
-class AdverbType(Enum):
-    MANNER = "manière"
-    FREQUENCY = "fréquence"
-    TIME = "temps"
-    PLACE = "place"
-    QUANTITY = "quantité"
-    NEGATION = "négation"
+    # define enum for types of adverbs in french
+    AdverbType = ["manière",
+                  "fréquence",
+                  "temps",
+                  "place",
+                  "quantité",
+                  "négation"]
 
-# define enum for types of conjuctions in french
-class ConjunctionType(Enum):
-    COORDINATING = "coordination"
-    SUBORDINATING = "subordination"
-    CORRELATIVE = "corrélation"
+    def prompt_word(self):
+        
+
+def import_json(filename):
+    with open(filename, 'r') as file:        
+        data = json.load(file)
+        return data
+
+class MyCLI(cmd.Cmd):
+    intro = 'France Fiche CLI\n'
+    prompt = '>>> '
+    file = "test.json"
+    word_list = []
+
+    def do_entry(self, arg):
+        new_word = {}
+        arguments=arg.split(' ')
+        for attribute in arguments:
+            key, value = attribute.split("=")
+            new_word[key] = value
+        print(new_word)
+        self.word_list.append(new_word)
+
+    def do_add(self, arg):
+        new_word = {}
+
+    def do_del(self, arg):
+        try:
+            print(f"Deleting {self.word_list[int(arg)]}")
+            self.word_list.pop(int(arg))
+        except IndexError:
+            print("Wrong Index")
+
+    def do_show(self, arg):
+        for word in self.word_list:
+            print('\n')
+            for elem in word.keys():
+                print(f"{elem} = {word[elem]}")
     
+    def do_save(self, arg):
+        categorized_words = {w_type: [] for w_type in grammar.WordType}
+        categorized_words['RANDOM'] = []
+        for word in self.word_list:
+            
+            try:
+                WordType = word.pop('WordType')
+                categorized_words[WordType].append(word)
+            except:
+                categorized_words["RANDOM"].append(word)
+        export_dicts_to_json(categorized_words)
+        self.word_list = []
+
+    def do_quit(self, arg):
+        """Exit the CLI"""
+        print('Quitting...')
+        return True
+
 # Create data class for storing french to english translation
 def export_dicts_to_json(dicts_list):
     """
@@ -89,33 +123,14 @@ def export_dicts_to_json(dicts_list):
     :return: None
     """
     timestamp = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
-    directory = f"json_exports/{timestamp}"
-    os.makedirs(directory, exist_ok=True)
-    for i, dictionary in enumerate(dicts_list):
-        filename = f"{directory}/dictionary_{i+1}.json"
+    base_dir = f"json_exports"
+    os.makedirs(base_dir, exist_ok=True)
+    for cathegory in dicts_list.keys():
+        directory = f"{base_dir}/{cathegory}"
+        os.makedirs(directory, exist_ok=True)
+        filename = f"{directory}/words_{timestamp}.json"
         with open(filename, "w") as f:
-            json.dump(dictionary, f)
-
-def import_json(filename):
-    with open(filename, 'r') as file:        
-        data = json.load(file)
-        return data
-
-class MyCLI(cmd.Cmd):
-    intro = 'Welcome to my CLI. Type help or ? to list commands.\n'
-    prompt = '> '
-    file = None
-
-# define function exporting the python dictionaries into json dictonaries and saving them in the file with directory timestamp
-
-    def do_hello(self, arg):
-        """Say hello to the user"""
-        print(f'Hello, {arg}!')
-
-    def do_quit(self, arg):
-        """Exit the CLI"""
-        print('Quitting...')
-        return True
+            json.dump(dicts_list[cathegory], f)
 
 if __name__ == '__main__':
     MyCLI().cmdloop()
